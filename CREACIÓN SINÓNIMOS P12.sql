c8c9565e89a00f75f7b8fdf2d3956b146aef485e
Venec0 <joeljesus30030@gmail.com>
2022-11-26 20:30:43 -0300

    TERMINANDO CASOS GUÍA N°12, AGREGANDO PERMISOS Y GRANTS PARA P12.

---
+++ CREACIÓN SINÓNIMOS P12.sql
@@ -0,0 +1,11 @@
+CREATE PUBLIC SYNONYM syn_cliente FOR MDY2131_P12_1.cliente
+;
+
+CREATE PUBLIC SYNONYM prestamo_titular FOR MDY2131_P12_1.credito_cliente
+;
+
+CREATE PUBLIC SYNONYM articulo_inversion_titular FOR MDY2131_P12_1.producto_inversion_cliente
+;
+
+CREATE PUBLIC SYNONYM cupo_prestamo_titular FOR MDY2131_P12_1.cuota_credito_cliente
+;
--- GUÍA 12.sql
+++ GUÍA 12.sql
@@ -3,23 +3,25 @@
 SELECT
     TO_CHAR(cli.numrun, '99G999G999')||'-'||INITCAP(cli.dvrun) AS "RUN CLIENTE",
     INITCAP(cli.pnombre||' '||cli.snombre||' '||cli.appaterno||' '||cli.apmaterno) AS "NOMBRE CLIENTE",
-    TO_CHAR(cli.fecha_nacimiento, 'dd "de" MONTH"') AS "DIA DE CUMPLEAÑOS", --= EXTRACT(MONTH FROM SYSDATE+1)"AS DÍA DE CUMPLEAÑOS"
+    TO_CHAR(cli.fecha_nacimiento, 'dd "de" MONTH"') AS "DÍA DE CUMPLEAÑOS",
     cli.direccion||', '||com.nombre_comuna AS "DIRECCIÓN CLIENTE"
 FROM mdy2131_p12_1.cliente cli
     JOIN comuna com ON cli.cod_comuna = com.cod_comuna
-    JOIN provincia pro ON com.cod_provincia = pro.cod_provincia
-    JOIN region reg ON pro.cod_region = reg.cod_region
+                    AND cli.cod_provincia = com.cod_provincia
+                    AND cli.cod_region = com.cod_region
+    WHERE EXTRACT(MONTH FROM cli.fecha_nacimiento) = EXTRACT(MONTH FROM SYSDATE)+1
 GROUP BY cli.numrun, cli.dvrun
         ,cli.pnombre, snombre, cli.appaterno, cli.apmaterno
         ,cli.fecha_nacimiento
         ,cli.direccion 
         ,com.nombre_comuna
-ORDER BY "DIA DE CUMPLEAÑOS" ASC ,cli.appaterno ASC
+ORDER BY "DÍA DE CUMPLEAÑOS" ASC ,cli.appaterno ASC
 ;
 
---CORREGIR GROUP BY
---HACER QUE SE EJECUTE LA SENTENCIA DE "DIA DE CUMPLEAÑOS TENIENDO EN CUENTA UN MES DESPUÉS DE EJECUCIÓN"
+
+--CASO N°2
 
 SELECT
-*
-FROM mdy2131_p12_1.comuna
+    TO_CHAR
+FROM mdy2131_p12_1.cliente cli
+;
